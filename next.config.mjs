c9596586c9663f6073aa569bfc1e6990db9a7f9e
AXUNDA <charlesemmanuelazunda@gmail.com>
2024-06-21 08:34:33 +0100

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-   missingSuspenseWithCSRBailout: false,
-};
+// const nextConfig = {
+//   experimental: {
+//     missingSuspenseWithCSRBailout: false,
+//   },
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+module.exports = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
